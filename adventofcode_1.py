a6fd01ffef74df79ebaf2dbd27d8e40739e54ed1
marlen625 <63655366+marlen625@users.noreply.github.com>
2021-12-05 00:20:28 +0100

    Add files via upload

---
+++ adventofcode_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 11:08:42 2021
+
+@author: marlen625
+"""
+
+"""
+part 1
+"""
+
+textFile = open("adventofcode_1.txt", "r").readlines()
+depthList = list(map(int, textFile))
+
+count = 0
+for index, depth in enumerate(depthList):
+    if index != 0 and depth > depthList[index-1]:
+        count += 1
+
+print(count)
+
+"""
+part 2
+"""
+
+count = 0
+for index, depth in enumerate(depthList):
+    if index < len(depthList) - 3 and depthList[index + 3] > depth:
+        count += 1
+
+print(count)
